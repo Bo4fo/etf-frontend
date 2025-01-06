1d27e3b55d3d2741a2592f3cb7703e4b9e77666b
Bo4fo <philipboafo21@gmail.com>
2025-01-06 17:00:59 +0000

    update 3

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,7 +6,7 @@ const Header = () => {
     <header className="bg-blue-600 text-white p-4">
       <div className="container mx-auto flex justify-between items-center">
         <Link to="/" className="text-2xl font-bold">
-          My Ecommerce
+          Eternal-feels
         </Link>
         <nav>
           <ul className="flex space-x-4">
